e74f0f4ca02dfcd5cd4034d632b84f9c195dfba1
momothereal <momothereal.mc@gmail.com>
2018-08-20 02:15:11 -0400

    Revert slider changes, .env

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import os
+
 from rmc.web import WebApp
 
 if __name__ == '__main__':
-    web_app = WebApp()
+    web_app = WebApp(
+        host=os.environ.get("RMC_HOST", default="0.0.0.0"),
+        port=os.environ.get("RMC_PORT", default=8080),
+        redis_host=os.environ.get("REDIS_HOST", default="127.0.0.1"),
+        redis_port=os.environ.get("REDIS_PORT", default=6379)
+    )
     web_app.run()
