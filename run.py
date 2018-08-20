120df57587170654485cb229c26223aab6a9ca2d
momothereal <momothereal.mc@gmail.com>
2018-08-20 02:16:50 -0400

    Environ types

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from rmc.web import WebApp
 if __name__ == '__main__':
     web_app = WebApp(
         host=os.environ.get("RMC_HOST", default="0.0.0.0"),
-        port=os.environ.get("RMC_PORT", default=8080),
+        port=int(os.environ.get("RMC_PORT", default=8080)),
         redis_host=os.environ.get("REDIS_HOST", default="127.0.0.1"),
-        redis_port=os.environ.get("REDIS_PORT", default=6379)
+        redis_port=int(os.environ.get("REDIS_PORT", default=6379))
     )
     web_app.run()
